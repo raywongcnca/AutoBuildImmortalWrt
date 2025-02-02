675c50b0449def6541aa23e9345ef6ec948ffbd7
raywongcnca <raywongbcca@outlook.com>
2025-02-01 20:45:02 -0800

    Update 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -18,7 +18,7 @@ else
 fi
 # 无需判断网卡数量 因为glinet是多网口
 uci set network.lan.ipaddr='192.168.6.1'
-echo "set 192.168.8.1 at $(date)" >> $LOGFILE
+echo "set 192.168.6.1 at $(date)" >> $LOGFILE
 # 判断是否启用 PPPoE
 echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
 if [ "$enable_pppoe" = "yes" ]; then
